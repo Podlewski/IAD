97812833b9acf433a76019df05a677f56b37dec0
Karlosinio <karol.podlewski@hotmail.com>
2018-06-10 12:06:09 +0200

    Small changes

--- Rozgrzewka8/Program.py
+++ Rozgrzewka8/Program.py
@@ -13,7 +13,7 @@ def readFile(in_file):
     outX = []
     outY = []
     for row in reader:
-        i = list(map(float, row[0].split(";")))
+        i = list(map(float, row[0].split(" ")))
         outX.append(i[0:-1])
         outY.append(i[-1])
     return outX, outY
@@ -24,12 +24,37 @@ def k(d, sigm):
     return float(1 / (m.sqrt(2 * m.pi) * sigm) * exp)
 
 
+def f(x, arrC, arrS, arrW, w0):
+    sum = 0
+    for i in range(len(arrC)):
+        d = m.fabs(x - arrC[i])
+        sum += arrW[i] * k(d, arrS[i])
+    return w0 + sum
+
+
+def f_single(x, c, s, w, w0):
+    d = m.fabs(x - c)
+    return w0 + w * k(d, s)
+
+
+def new_w(w, c, ):
+    alfa = 0.1
+    w = w - alfa * (1/c)
+    pass
+
+
+def q(arrX, arrY, arrC, arrS, arrW, w0):
+    n = len(arrX)
+    q = 1/2*n
+    pass
+
+
 file = open(sys.argv[1], "r")
 
-arrX, arrY = readFile(file)
+trrX, trrY = readFile(file)
 
-a = min(arrX)   # min X
-b = max(arrX)   # max X
+a = min(trrX)   # min X
+b = max(trrX)   # max X
 c = int(sys.argv[2])
 
 arrC = np.empty(c)
@@ -37,13 +62,11 @@ arrS = np.empty(c)
 arrW = np.empty(c)
 w0 = rand.uniform(-4, 4)
 
+
 for i in range(0, c):
     arrC[i] = rand.uniform(a, b)
-    arrS[i] = rand.uniform(0.1, 0.5)
+    arrS[i] = rand.uniform(0.5, 1)
     arrW[i] = rand.uniform(-4, 4)
-
-arrX = np.linspace(0, 10, 1001)
-arrY = np.zeros(1001)
 
 
 for i in range(len(arrC)):
@@ -62,6 +85,7 @@ for x in range(len(arrX)):
     arrY[x] = w0 + sum
 
 plt.plot(arrX, arrY, color='k')
+plt.scatter(trrX, trrY, color='g', s=5)
 
 plt.tight_layout()
 plt.grid()
