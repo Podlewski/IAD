2225ef6b23a024d3f89057d45a2e4d25f0265fb9
Karlosinio <karol.podlewski@hotmail.com>
2018-06-20 20:47:30 +0200

    Fixed to new program

--- Zadanie3/run.sh
+++ Zadanie3/run.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Zadanie 3.1
-./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -i 3 -s 0.1 -p Results/3_1_A
-# ./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -i 3 -p Results/3_1_B
-./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -i 3 -s 4 -p Results/3_1_C
+./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -s 0.25 -p Results/3_1_A
+./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -p Results/3_1_B
+./Program.py Data/train.txt Data/test.txt 1 11 21 31 41 -s 4 -p Results/3_1_C
 
 # Zadanie 3.2
-./Program.py Data/train.txt Data/test.txt 21 -i 3 -s 0.1 -pr Results/3_2_A
-./Program.py Data/train.txt Data/test.txt 21 -i 3 -pr Results/3_2_B
-./Program.py Data/train.txt Data/test.txt 21 -i 3 -s 4 -pr Results/3_2_C
+./Program.py Data/train.txt Data/test.txt 21 -s 0.25 -pr Results/3_2_A
+./Program.py Data/train.txt Data/test.txt 21 -pr Results/3_2_B
+./Program.py Data/train.txt Data/test.txt 21 -s 4 -pr Results/3_2_C
 
 # Zadanie 3.3
 for j in 1 6 11 16 21 26 31 36 41
 do
   for i in {0..11}
   do
-    ./Program.py Data/train.txt Data/test.txt $j -i 3 -sr Results/3_3
+    ./Program.py Data/train.txt Data/test.txt $j -sr Results/3_3
   done
 done
 
 # Zadanie 3.4
-./Program.py Data/train.txt Data/test.txt 21 -i 3 -ph Results/3_4
+./Program.py Data/train.txt Data/test.txt 21 -ph Results/3_4
